7059aa6858d970e34cd2ed786a0b72bc22b723d8
MárcioMachado2182 <marciojjb@gmail.com>
2024-07-01 16:48:47 -0300

    janela

---
+++ janela/janela_scroll.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import ttk
+
+# Cria a janela principal
+root = tk.Tk()
+#root.title("Minha Primeira GUI")
+root.geometry("70x21")
+
+# Adiciona um label
+message = tk.Label(root, text="Label 2 ", background="green", anchor="w")
+message.pack(side="left")
+
+message = tk.Label(root, text="Label 3 ", background="blue", anchor="w")
+message.pack(side="left")
+
+message = tk.Label(root, text="Label 1 ", background="red", anchor="w")
+message.pack(side="left")
+
+
+
+root.mainloop()
